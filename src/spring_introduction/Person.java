f692bfe681f52a761f5e949a11a35bd3b45852d5
jazzboy65 <alexis.gordeeff@gmail.com>
2022-02-27 22:22:18 +0500

    + SC 5,6

--- src/spring_introduction/Person.java
+++ src/spring_introduction/Person.java
@@ -2,11 +2,37 @@ package spring_introduction;
 
 public class Person {
     private Pet pet;
+    private String surname;
+    private int age;
 
-    public Person(Pet pet) {
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        System.out.println("Class Person: set surname");
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Class Person: set age");
+        this.age = age;
+    }
+
+    public Person() {
         System.out.println("Person bean is created");
+
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: set pet");
         this.pet = pet;
     }
+
 
     public void callYourPet() {
         System.out.println("Hello, my lovely Pet");
