39d8c2b46fc1fe987e69e79b22af833b4b835390
jazzboy65 <alexis.gordeeff@gmail.com>
2022-03-05 10:57:18 +0500

    + SC 8,9

--- src/spring_introduction/Dog.java
+++ src/spring_introduction/Dog.java
@@ -1,6 +1,24 @@
 package spring_introduction;
 
 public class Dog implements Pet {
+//    private String name;
+
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+
+    public void init() {
+        System.out.println("Class Dog: init method");
+    }
+
+    public void destroy() {
+        System.out.println("Class Dog: destroy method");
+    }
+
     public Dog() {
         System.out.println("Dog bean is created");
     }
