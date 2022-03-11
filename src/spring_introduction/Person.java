87ec1fa27a2f1f027060aee66a53040be8a14e47
jazzboy65 <alexis.gordeeff@gmail.com>
2022-03-11 21:39:41 +0500

    + SC 10-12

--- src/spring_introduction/Person.java
+++ src/spring_introduction/Person.java
@@ -1,9 +1,25 @@
 package spring_introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component("personBean")
 public class Person {
     private Pet pet;
     private String surname;
     private int age;
+
+//    @Autowired
+//    public Person(Pet pet) {
+//        System.out.println("Person bean is created");
+//        this.pet = pet;
+//    }
+
+       public Person() {
+        System.out.println("Person bean is created");
+
+    }
 
     public String getSurname() {
         return surname;
@@ -23,13 +39,10 @@ public class Person {
         this.age = age;
     }
 
-    public Person() {
-        System.out.println("Person bean is created");
 
-    }
-
-    public void setPet(Pet pet) {
-        System.out.println("Class Person: set pet");
+    @Autowired
+    public void anyMethodName(Pet pet) {
+        System.out.println("Class Person: anyMethodName");
         this.pet = pet;
     }
 
