2442c611341cdb710de49f2a15998dc719354629
jazzboy65 <alexis.gordeeff@gmail.com>
2022-03-13 22:06:42 +0500

    + SC 13,14

--- src/spring_introduction/Person.java
+++ src/spring_introduction/Person.java
@@ -1,25 +1,30 @@
 package spring_introduction;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
 @Component("personBean")
 public class Person {
+//    @Autowired
+//    @Qualifier("catBean")
     private Pet pet;
+    @Value("${person.surname}")
     private String surname;
+    @Value("${person.age}")
     private int age;
 
-//    @Autowired
-//    public Person(Pet pet) {
-//        System.out.println("Person bean is created");
-//        this.pet = pet;
-//    }
-
-       public Person() {
+    @Autowired
+    public Person(@Qualifier("dog") Pet pet) {
         System.out.println("Person bean is created");
-
+        this.pet = pet;
     }
+
+//       public Person() {
+//        System.out.println("Person bean is created");
+//    }
 
     public String getSurname() {
         return surname;
@@ -40,9 +45,10 @@ public class Person {
     }
 
 
-    @Autowired
-    public void anyMethodName(Pet pet) {
-        System.out.println("Class Person: anyMethodName");
+//    @Autowired
+//    @Qualifier("dog")
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: setPet");
         this.pet = pet;
     }
 
