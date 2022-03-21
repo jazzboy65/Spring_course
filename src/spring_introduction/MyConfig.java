9a5bd79f86b2cd0aeb4d4404fe9533e38a5dcb96
jazzboy65 <alexis.gordeeff@gmail.com>
2022-03-21 23:06:27 +0500

    + SC 17,18

--- src/spring_introduction/MyConfig.java
+++ src/spring_introduction/MyConfig.java
@@ -1,10 +1,21 @@
 package spring_introduction;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 
 @Configuration
-@ComponentScan("spring_introduction")
+@PropertySource("classpath:myApp.properties")
+//@ComponentScan("spring_introduction")
 
 public class MyConfig {
+
+    @Bean
+    @Scope("singleton")
+    public Pet catBean() {
+        return new Cat();
+    }
+
+    @Bean
+    public Person personBean() {
+        return new Person(catBean());
+    }
 }
